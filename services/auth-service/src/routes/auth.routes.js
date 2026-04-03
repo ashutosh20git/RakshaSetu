e490139879c8d138df5c11105cbe6f017255f36b
Ashutosh Dubey <ad417596@gmail.com>
2026-04-03 14:13:36 +0530

    add JWT auth middleware

--- services/auth-service/src/routes/auth.routes.js
+++ services/auth-service/src/routes/auth.routes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth.controller');
+const { verifyToken, authorizeRoles } = require('../middleware/auth.middleware');
+
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/me', verifyToken, (req, res) => {
+  res.json({ message: 'Token valid', user: req.user });
+});
+
+router.get('/admin', verifyToken, authorizeRoles('AUTHORITY'), (req,res)=>{
+    res.json({ message: 'Welcome authority' });
+});
 
 module.exports = router;
